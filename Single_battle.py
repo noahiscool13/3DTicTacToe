8552e6f6085295a9a818050c5e3d07ba8dbe7641
noahiscool13 <noah@gamestoplay.nl>
2018-01-23 22:17:27 +0100

    Implemented a multi core version of the alpha beta optimization and made the single battle safe by checking for __name__ == __main__

--- Single_battle.py
+++ Single_battle.py
@@ -1,14 +1,20 @@
 from Game import *
 from copy import deepcopy
-from human_inteligence import AI as AI1
-from basic_alpha_beta import AI as AI2
+from multy_core_a_b import AI as AI1
+from human_inteligence import AI as AI2
 
-game = Game()
+display = 1
 
-while not game.check_board():
-    if game.player == "x":
-        game.move(AI1(deepcopy(game)))
-    else:
-        game.move(AI2(deepcopy(game)))
-game.print_board()
-print(game.check_board())
+if __name__ == '__main__':
+    game = Game()
+    while not game.check_board():
+        if game.player == "x":
+            game.move(AI1(deepcopy(game)))
+        else:
+            game.move(AI2(deepcopy(game)))
+        if display:
+            game.print_board()
+            print("Last move was:", game.moves[-1])
+    game.print_board()
+    print(game.check_board())
+    print(game.moves)
